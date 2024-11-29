e6730f2b27080d4b4566b47bdb09e32d4bdc8cb9
chasethesky79 <seshadri.bharath@yahoo.com>
2024-11-29 13:55:37 -0600

    Implemented the domain and repository classes

---
+++ src/main/java/com/polarbookshop/orderservice/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.polarbookshop.orderservice.repository;
+
+import com.polarbookshop.orderservice.domain.Order;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
+}
