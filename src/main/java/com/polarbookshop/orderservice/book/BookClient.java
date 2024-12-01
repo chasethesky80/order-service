20a500248621ab0d4544d3ccb3d66c72310d43de
chasethesky79 <seshadri.bharath@yahoo.com>
2024-12-01 10:49:24 -0600

    Order service changes

---
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -0,0 +1,29 @@
+package com.polarbookshop.orderservice.book;
+
+import java.time.Duration;
+
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Component
+public class BookClient {
+
+	private static final String BOOKS_ROOT_API = "/books/";
+	private final WebClient webClient;
+
+	public BookClient(WebClient webClient) {
+		this.webClient = webClient;
+	}
+
+	public Mono<Book> getBookByIsbn(String isbn) {
+		return webClient
+				.get()
+				.uri(BOOKS_ROOT_API + isbn)
+				.retrieve()
+				.bodyToMono(Book.class);
+	}
+}
